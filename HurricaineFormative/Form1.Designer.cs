f74fe76944428cd2974934d8f698a592a65c9e42
deonara447 <deonara447@ed.amdsb.ca>
2018-10-23 14:04:51 -0400

    Add project files.

---
+++ HurricaineFormative/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace HurricaineFormative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.catInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Location = new System.Drawing.Point(187, 190);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(70, 25);
+            this.categoryLabel.TabIndex = 0;
+            this.categoryLabel.Text = "label1";
+            // 
+            // catInput
+            // 
+            this.catInput.Location = new System.Drawing.Point(504, 183);
+            this.catInput.Name = "catInput";
+            this.catInput.Size = new System.Drawing.Size(100, 31);
+            this.catInput.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(323, 413);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(70, 25);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.catInput);
+            this.Controls.Add(this.categoryLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.TextBox catInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ HurricaineFormative/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HurricaineFormative
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string category = catInput.Text;
+
+            switch (category)
+            {
+                case "1":
+                    outputLabel.Text = "number 1";
+                    break;
+                case "2":
+                    outputLabel.Text = "number 2";
+                    break;
+                case "3":
+                    outputLabel.Text = "number 3";
+                    break;
+                case "4":
+                    outputLabel.Text = "number 4";
+                    break;
+                case "5":
+                    outputLabel.Text = "number 5";
+                    break;
+            
+                default:
+                    outputLabel.Text = "default";
+                    break;
+
+
+
+            }
+
+        }
+    }
+}
